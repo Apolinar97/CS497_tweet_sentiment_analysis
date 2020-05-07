f2edb4cef57c1987ad182d4503d7ef592ea2576b
Apolinar Camacho <camacho.apolinar97@gmail.com>
2020-05-06 19:27:20 -0700

    renamed app.py to server.py

--- webservice/server.py
+++ webservice/server.py
@@ -6,7 +6,7 @@ import numpy as np
 import nltk
 nltk.download('stopwords')
 import re
-
+from pre_process import validate_tweet, process_tweet
 from nltk.corpus import stopwords
 from flask import Flask, jsonify, render_template, render_template_string
 from sklearn.preprocessing import LabelEncoder
@@ -18,29 +18,6 @@ app.config['DEBUG'] = True
 with open('token.pkl','rb') as token_file:
   tkn = pickle.load(token_file)
 
-def norm_tweet(tweet):
-  tweet = tweet.lower()
-  tweet = re.sub('(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)', ' ', tweet)
-  tweet = re.sub('[^a-z]+', ' ', tweet)
-  return tweet
-
-def remove_stopwords_for_analysis(tweet):
-  stop_words = stopwords.words('english')
-  white_list = ['no', 'not']
-  words_tweet = tweet.split()
-  clean_tweet = [word for word in words_tweet if(word not in stop_words or word in white_list)]
-  return ' '.join(clean_tweet)
-
-
-def process_tweet(tweet):
-  tweet = norm_tweet(tweet)
-  return remove_stopwords_for_analysis(tweet)
-
-def validate_tweet(tweet):
-  if (not tweet or not isinstance(tweet,str)):
-    return False
-  else:
-    return True
 
 
 @app.route('/')
